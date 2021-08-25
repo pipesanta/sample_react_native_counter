2f612af6192e7fc86185362d3ac224ebe1774eb1
= <=>
2021-08-24 21:48:30 -0500

    basic counter with usestate and button

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import { HelloWorldScreenComponent } from './src/screen/hello-world.screen';
+// import { HelloWorldScreenComponent } from './src/screen/hello-world.screen';
+import { CounterScreenComponent } from './src/screen/counter.screen';
 
 export default function App() {
 
   return (
-    <HelloWorldScreenComponent />
+    // <HelloWorldScreenComponent />
+    <CounterScreenComponent />
   )
 
 
